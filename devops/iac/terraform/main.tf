ec28cf46334a91cb56db843cd1bc88244723ee80
Leonardo Gama <leonardo.lgama@gmail.com>
2025-02-27 23:17:52 +0100

    Installing and configuring a simple django project with Ansible

--- devops/iac/terraform/main.tf
+++ devops/iac/terraform/main.tf
@@ -17,12 +17,6 @@ resource "aws_instance" "app_server" {
     ami           = "ami-03fd334507439f4d1"
     instance_type = "t2.micro"
     key_name      = "personal-connection"
-    # user_data     = <<-EOF
-    #     #!/bin/bash
-    #     cd /home/ubuntu
-    #     echo "<h1>Hello, World by Terraform!</h1>" > index.html
-    #     nohup busybox httpd -f -p 8080 &
-    # EOF
     tags = {
         Name = "Terraform Ansible Python"
     }
