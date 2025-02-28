4b59abedb993e6eee6a1ae55bf85721ef6213a88
Leonardo Gama <leonardo.lgama@gmail.com>
2025-02-28 23:24:20 +0100

    Infra for prod

---
+++ devops/iac/env/prod/terraform/main.tf
@@ -0,0 +1,12 @@
+module "aws_prod" {
+    source          = "../../../infra"
+    instance_type   = "t2.micro"
+    aws_region      = "eu-west-1"
+    key             = "../.ssh/id_rsa_prod"
+    security_group  = "general_access_prod"
+    instance_name   = "app_server_prod"
+}
+
+output "ip_dev" {
+    value = module.aws_prod.public_ip
+}
